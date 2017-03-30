8b1e2de076a8bfa2f0e66448a317568e9d70d799
namanksharma <sharmanaman09@gmail.com>
2017-03-31 04:22:14 +0530

    Update settings.py

--- weather_info/settings.py
+++ weather_info/settings.py
@@ -80,8 +80,9 @@ WSGI_APPLICATION = 'weather_info.wsgi.application'
 
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = { 'default': dj_database_url.config() }
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
 
 
 # Internationalization

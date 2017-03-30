5146155d74aeedd8b6b7249969a5d5f85dedc9e9
namanksharma <sharmanaman09@gmail.com>
2017-03-31 04:54:35 +0530

    Update urls.py

--- weather_info/urls.py
+++ weather_info/urls.py
@@ -16,6 +16,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from weather_info import views
 from weather_info import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),

957f130f7de5ba01ab7a38347f55d77aaf1b713c
namanksharma <sharmanaman09@gmail.com>
2017-03-31 04:45:12 +0530

    Update settings.py

--- weather_info/settings.py
+++ weather_info/settings.py
@@ -102,10 +102,10 @@ USE_TZ = False
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/staticfiles/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'staticfiles'),
 )
